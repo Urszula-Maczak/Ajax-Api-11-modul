bdc6279019a8c6e5179e29948744ee6bb80308c2
ursula <ulma83@poczta.fm>
2017-04-23 14:34:20 +0200

    create Random quote machine

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
-var url = 'http://api.icndb.com/jokes/random';
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function() {
-	getJoke();
-});
-var paragraph = document.getElementById('joke');
+//Metoda Ajax w jQuery - wersja obszerna
+/*$.ajax({
+ dataType: "json",
+ url: quoteUrl,
+ data: null,
+ success: createTweet
+});*/
 
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function() {
-		var response = JSON.parse(xhr.response);
-		paragraph.innerHTML = response.value.joke;
-	});
-	xhr.send();
+//Wersja krótsza
+function getQuote() {
+	$.getJSON(quoteUrl, createTweet);
+}	
+
+function createTweet(input) {
+	if (!input.quoteAuthor.length) {
+		input.quoteAuthor = "Unknown author";
+	}
+
+	var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+	if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote').text(input.quoteText);
+		$('.author').text("Author: " + input.quoteAuthor);
+		$('.tweet').attr('href', tweet);
+	}
 }
 
-getJoke();
+$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
+	})
+});
+
